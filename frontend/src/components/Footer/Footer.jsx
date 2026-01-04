b6435911dcdb6e1f91d30527ca95d4d2669ce3bc
heat-treat <tastecode.1525@gmail.com>
2026-01-04 13:12:09 -0600

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,25 +7,32 @@ import { FaFacebookF, FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="footer" id="footer">
-      
       <hr />
       <div className="footer-content">
         {/* Left - Logo & Description */}
         <div className="footer-content-left">
-          <a href="#top"><img src={assets.logo_hat} className="logo" alt="Heat & Treat Logo" /></a>
+          <a href="#top">
+            <img
+              src={assets.logo_hat}
+              className="logo"
+              alt="Heat & Treat Logo"
+            />
+          </a>
           <p>
             Experience The Perfect Blend of Bold Flavors & Fiery Passion. Made
             Fresh, Served Hot — The Ultimate Destination For Heat That Treats!
           </p>
           <div className="footer-social-icons">
-            <a className="fb"
+            <a
+              className="fb"
               href="https://www.facebook.com/people/%F0%9D%99%83%F0%9D%99%80%F0%9D%98%BC%F0%9D%99%8F-%F0%9D%98%BC%F0%9D%99%89%F0%9D%98%BF-%F0%9D%99%8F%F0%9D%99%8D%F0%9D%99%80%F0%9D%98%BC%F0%9D%99%8F/61581888510141/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaFacebookF />
             </a>
-            <a className="loc"
+            <a
+              className="loc"
               href="https://maps.app.goo.gl/5Urheg8tL1xRVCpY9"
               target="_blank"
               rel="noopener noreferrer"
@@ -55,7 +62,7 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>Contact Us</h2>
           <ul>
-            <li>
+            {/* <li>
               <MdOutgoingMail className="icon gmail" />
               <a
                 href="https://mail.google.com/"
@@ -64,10 +71,21 @@ const Footer = () => {
               >
                 tastecode.1525@gmail.com
               </a>
+            </li> */}
+
+            <li>
+              <MdOutgoingMail className="icon gmail" />
+              <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=tastecode.1525@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                tastecode.1525@gmail.com
+              </a>
             </li>
             <li>
               <FaPhoneAlt className="icon phone" />
-              <a href="tel:+8801635646047">+880 1635-646047</a>
+              <a href="tel:+8801334218173">+880 1635-646047</a>
             </li>
             <li>
               <FaPhoneAlt className="icon phone" />
