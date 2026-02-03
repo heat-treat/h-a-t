a2936f17f9d5c89ed1d5951ca01d9012f9328352
heat-treat <tastecode.1525@gmail.com>
2026-02-03 11:46:50 -0600

    Delete backend/routes/dashboardRoute.js

--- backend/routes/dashboardRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboardStats } from "../controllers/dashboardController.js";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/stats", getDashboardStats);
-
-export default dashboardRouter;
